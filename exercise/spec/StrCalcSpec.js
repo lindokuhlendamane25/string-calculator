a7803186324bf48a67f48c299e76cc5327031250
lindokuhlendamane25 <lindokuhlengwadla@gmail.com>
2019-09-19 16:00:49 +0200

    Update StrCalcSpec.js

--- exercise/spec/StrCalcSpec.js
+++ exercise/spec/StrCalcSpec.js
@@ -1,18 +1,21 @@
+describe("Add", function() {
+  it("should return 0 when the string is empty", function() {
+    expect(add("")).toBe(0);
+  });
+  it("should add two string numbers", function() {
+    expect(add("3,2")).toEqual(5);
+  });
+  it("should allow the Add method to handle new lines between numbers", function() {
+    expect(add("1\n2,3")).toEqual(6);
+  });
 
-describe("Add", function(){
-    it("should return 0 when the string is empty" , function(){
-        expect(add("")).toBe(0);
-    })
-     it("should add two string numbers", function(){
-      expect(add("7,2")).toEqual(9)
-     })
-     it("should Support different delimiters", function(){
-        expect(add("//;\n1;2")).toEqual(6) 
-     })
-     it("should allow the Add method to handle new lines between numbers",function(){
-         expect(add("1\n2,3")).toEqual(6)
-     })
-     it("should throw an exception if there are negative numbers", () => {
-            expect(() => { add("-3,1") }).toThrowError();
-       })
-})
+  it("should support different delimeters", function() {
+    expect(add("//;\n1;2")).toEqual(NaN);
+  });
+
+  it("should throw an exception if there are negative numbers", function() {
+    expect(function() {
+      throw "-3, 1";
+    }).toThrow();
+  });
+});
